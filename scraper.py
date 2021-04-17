d18ae3e0bd6658776ee8d4e815cb976e5d9b9831
Chancylvania <crmesser1@comcast.net>
2021-04-17 09:58:56 -0400

    Added COVID scraping and initial new user

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 from ScraperKeys import weather_api_public_key
 from flask import Flask 
 from flask import request
@@ -6,22 +6,25 @@ from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
 import requests
-import json  
+import json
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 authenticate = HTTPBasicAuth()
 
-#dictionary holding the users for the API
+# dictionary holding the users for the API
 users = {
-    "admin" : generate_password_hash("Secret"),
+    "admin": generate_password_hash("Secret"),
 }
 
-#code used to verfiy the possible users for the API
+
+# code used to verify the possible users for the API
 @authenticate.verify_password
 def verify_password(username, password):
     if username in users and \
             check_password_hash(users.get(username), password):
         return username
+
 
 @app.route('/')
 @authenticate.login_required
@@ -34,11 +37,11 @@ def index():
                         <li> And even add new Flask Usernames and Passwords </li>
                     </u1>"""
 
-#get details on Weather by city
-@app.route('/Weather/<city>', methods = ['GET']) #Will accept HTTP GET requests
+
+# get details on Weather by city
+@app.route('/Weather/<city>', methods=['GET']) #Will accept HTTP GET requests
 @authenticate.login_required
 def get_weather(city):
-
     place = city
     r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={place}&appid={weather_api_public_key}")
     data = r.json()
@@ -50,24 +53,44 @@ def get_weather(city):
     json_string = json.dumps(weather_data)
     return json_string
 
-#get details on COVID by state from this website: https://www.worldometers.info/coronavirus/country/us/
-@app.route('/COVID/<state>', methods = ['GET']) #You will need to look up how scrap data from this website: https://realpython.com/beautiful-soup-web-scraper-python/
+
+# get details on COVID by state from this website: https://www.worldometers.info/coronavirus/country/us/
+@app.route('/COVID/<state>', methods=['GET']) #Will accept HTTP GET requests
 @authenticate.login_required
 def get_covid_data(state):
+    r = requests.get(f"https://www.worldometers.info/coronavirus/usa/{state}")
+    soup = BeautifulSoup(r.content, 'html.parser')
+    results = soup.find_all(id='maincounter-wrap')
+    i = 0
+    for result in results:
+        num = result.find('span')
+        if i == 0:
+            cases = num.text.strip()
+        elif i == 1:
+            deaths = num.text.strip()
+        elif i == 2:
+            recovered = num.text.strip()
+        #print(num.text.strip(), end='\n')
+        i += 1
+    #print(cases)
+    #print(deaths)
+    #print(recovered)
+    covid_data = {"state": state, "total cases": cases, "deaths": deaths, "total recovered": recovered}
+    json_string = json.dumps(covid_data)
+    print(json_string)
+    return json_string
 
-    location = state
 
-
-    return json.dumps({"state":location}) #simply just returns the state is being queried
-
-#Add new user
-@app.route('/Update/', methods = ['POST']) #Will only accept HTTP POST requests
+# Add new user
+@app.route('/Update/', methods=['POST']) #Will only accept HTTP POST requests
 @authenticate.login_required
-def post_user_pass():
-
-    #Make sure to insert new user into the dictionary of users
-    return """ <h1> A temporary placeholder response 3 </h1> """
+def post_user_pass(user, passw):
+    # Make sure to insert new user into the dictionary of users
+    users.update({user:passw})
+    newUser = {user:passw}
+    print(newUser)
+    return newUser
 
 
 if __name__== "__main__":
-    app.run(host='127.0.0.1', port = 8081, debug = True)
+    app.run(host='127.0.0.1', port=8081, debug = True)
--- services.py
+++ services.py
@@ -1,8 +1,8 @@
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 from ServicesKeys import marvel_api_public_key
 from ServicesKeys import marvel_api_private_key
 from flask import Flask
-from flask import abort 
+from flask import abort
 from flask import request
 from requests.auth import HTTPBasicAuth
 from flask_httpauth import HTTPBasicAuth
@@ -11,16 +11,16 @@ from werkzeug.security import check_password_hash
 import requests
 import json
 import time
-import hashlib  
-
+import hashlib
 
 app = Flask(__name__)
 authenticate = HTTPBasicAuth()
 
-#dictionary holding the users for the API
+# dictionary holding the users for the API
 users = {
-    "admin" : generate_password_hash("secret"),
+    "admin": generate_password_hash("secret"),
 }
+
 
 @authenticate.verify_password
 def verify_password(username, password):
@@ -28,77 +28,99 @@ def verify_password(username, password):
             check_password_hash(users.get(username), password):
         return username
 
+
 @app.route('/')
 @authenticate.login_required
 def index():
-
     return f"""<h1>Welcome {authenticate.current_user()} to the Services API!</h1>
             <p>You can access the Marvel API from this API!</p>"""
 
-@app.route('/Marvel') #Will only accpet HTTP GET requests
+
+@app.route('/Marvel')  # Will only accept HTTP GET requests
 @authenticate.login_required
 def get_marvel_stories():
     if 'story' in request.args:
-        #setting up for requesting from the marvel API using server-side authentication
+        # setting up for requesting from the marvel API using server-side authentication
         timestamp = str(time.time())
         str2hash = timestamp + marvel_api_private_key + marvel_api_public_key
         md5hash = hashlib.md5(str2hash.encode()).hexdigest()
 
-        r = requests.get(f"http://gateway.marvel.com/v1/public/stories/{request.args['story']}?apikey={marvel_api_public_key}&hash={md5hash}&ts={timestamp}")
+        r = requests.get(
+            f"http://gateway.marvel.com/v1/public/stories/{request.args['story']}?apikey={marvel_api_public_key}&hash={md5hash}&ts={timestamp}")
 
         data = r.json()
 
         return data["data"]["results"][0]["description"]
 
     else:
-        abort(400) #return an abort message with HTTP status code 400 (Bad Request)
+        abort(400)  # return an abort message with HTTP status code 400 (Bad Request)
 
-#The following routes will make post requests which will be turned into HTTP GET requests by the scraper
-@app.route('/Weather/<city>', methods = ['POST']) #Will accept HTTP POST requests
+
+# The following routes will make post requests which will be turned into HTTP GET requests by the scraper
+@app.route('/Weather/<city>', methods=['POST'])  # Will accept HTTP POST requests
 @authenticate.login_required
 def post_weather(city):
-
-    #get the username and password for the scraper from the request body (the -d parameter)
+    # get the username and password for the scraper from the request body (the -d parameter)
     scraper_username = request.form["user"]
     scraper_password = request.form["pass"]
 
-    r = requests.get(f'http://127.0.0.1:8081/Weather/{city}', auth = (scraper_username, scraper_password))
-    if r.status_code  == 200:
+    r = requests.get(f'http://127.0.0.1:8081/Weather/{city}', auth=(scraper_username, scraper_password))
+
+    if r.status_code == 200:
         return r.json()
-    
+
     elif r.status_code == 401:
         abort(401)
-    
+
     else:
         abort(400)
 
 
-
-@app.route('/COVID/<state>', methods = ['POST'])
+@app.route('/COVID/<state>', methods=['POST'])
 @authenticate.login_required
 def post_covid_data(state):
-    #get the username and password for the scraper from the request body (the -d parameter)
+    # get the username and password for the scraper from the request body (the -d parameter)
     scraper_username = request.form["user"]
     scraper_password = request.form["pass"]
 
-    r = requests.get(f'http://127.0.0.1:8081/COVID/{state}', auth = (scraper_username, scraper_password))
-    
-    #This might or might not need to be changed depending how the scraping works
-    if r.status_code  == 200:
-        return r.json()
-    
+    r = requests.get(f'http://127.0.0.1:8081/COVID/{state}', auth=(scraper_username, scraper_password))
+
+    # This might or might not need to be changed depending how the scraping works
+    if r.status_code == 200:
+        print(r.content)
+        print(r.json())
+        return r.content
+
     elif r.status_code == 401:
         abort(401)
-    
+
     else:
         abort(400)
 
-@app.route('/Update', methods = ['POST']) #Will only accept HTTP POST requests
+
+@app.route('/Update', methods=['POST'])  # Will only accept HTTP POST requests
 @authenticate.login_required
 def post_user_pass():
+    "user=scrape_user?pass=scrape_pass&new_user=user2&new_pass=pass2"
+    scraper_username = request.form["user"]
+    scraper_password = request.form["pass"]
+    newUser = request.form["new_user"]
+    newPass = request.form["new_pass"]
 
-    
-    return """ <h1> A temporary placeholder response 3 </h1> """
+    r = requests.get(f'http://127.0.0.1:8081/Update/', auth=(scraper_username, scraper_password))
+    if r.status_code == 200:
+        print(newUser)
+        print(newPass)
+        return "success"
+    """
+    elif r.status_code == 401:
+        abort(401)
 
-if __name__== "__main__":
-    app.run(host='127.0.0.1', port = 3000, debug = True)
+    else:
+        abort(400)
+    """
+    return "failure"
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=3000, debug=True)
